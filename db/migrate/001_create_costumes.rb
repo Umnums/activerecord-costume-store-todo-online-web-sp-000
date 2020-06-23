2239235acdd27074f7096ddf3b44ce8322c6ca10
Joseph Kao <josephtkao@gmail.com>
2020-06-23 16:12:00 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,7 +5,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :name
       t.float :price
       t.string :size
-      t.string :url
+      t.string :image_url
 
       t.timestamps
     end
