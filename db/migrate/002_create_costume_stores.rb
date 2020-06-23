8933e1ffd6ba0cb0ad1e0032e7efe02cefc7986f
Joseph Kao <josephtkao@gmail.com>
2020-06-23 16:07:06 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+# Create a class and inherit from ActiveRecord::Migration
+class CreateCostumesStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumestores do |t|
+      t.string :name
+      t.string :location
+      t.integer :inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time 
+    end
+  end
+end
